59bc8fb831933393a85fcd7042ccff62455a7800
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-12-13 14:28:32 +0200

    finished test

--- src/index.js
+++ src/index.js
@@ -16,7 +16,51 @@ const ROMAN_GLYPHS = [
 
 class RomanNumeralConverter {
   convert(value) {
-
+    if (isNaN(parseInt(value, 10))) {
+      value = value.toUpperCase()
+      if (value.indexOf('-')===0) {
+        return -this.convertRomanToArabic(value.substr(1))
+      }
+      return this.convertRomanToArabic(value)
+    } else {
+      if (value<0) {
+        return '-'+this.convertArabicToRoman(Math.abs(value))
+      }
+      return this.convertArabicToRoman(value)
+    }
+  }
+  convertRomanToArabic(value) {
+    if (!this.isRomanNumber(value)) {
+      throw new Error("Value is not a Roman Number")
+    }
+    let result = 0
+    while(value) {
+      ROMAN_GLYPHS.forEach(([num,glyph])=> {
+        const test = new RegExp(`^${glyph}`)
+        if (test.test(value)) {
+          result += num
+          value = value.replace(test, '')
+        }
+      })
+    }
+    return result
+  }
+  convertArabicToRoman(value) {
+    if (Math.abs(value>3999)) {
+      throw new Error("Can't convert values greater than 3999")
+    }
+    let result = ''
+    ROMAN_GLYPHS.forEach(([num,glyph])=> {
+      while(value>=num) {
+        result += glyph
+        value -= num
+      }
+    })
+    return result
+  }
+  isRomanNumber(value) {
+    const glyphs = ROMAN_GLYPHS.map(([num,glyph])=>glyph)
+    return value!=='' && value.split('').every(char=>glyphs.includes(char))
   }
 }
 export default RomanNumeralConverter
