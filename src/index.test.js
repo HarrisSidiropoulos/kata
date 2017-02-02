c416abdc4a0a618c58df57bc2651f76c1600d35a
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-02-02 14:46:40 +0200

    add eslint

--- src/index.test.js
+++ src/index.test.js
@@ -1,21 +1,21 @@
-import {expect} from 'chai';
+import { expect, } from 'chai';
 import RomanNumeralConverter from './index';
 
 describe('RomanNumerals', () => {
-  const converter = new RomanNumeralConverter()
+  const converter = new RomanNumeralConverter();
 
   describe('class', () => {
-    it('should not be undefined', ()=> {
+    it('should not be undefined', () => {
       expect(converter).to.not.be.an('undefined');
-    })
-    it('should be an object', ()=> {
+    });
+    it('should be an object', () => {
       expect(converter).to.be.an('object');
-    })
+    });
     describe('method convert', () => {
-      it('should not be undefined', ()=> {
+      it('should not be undefined', () => {
         expect(converter.convert).to.not.be.an('undefined');
       });
-      it('should be a function', ()=> {
+      it('should be a function', () => {
         expect(converter.convert).to.be.a('function');
       });
     });
