e40479c41f32cdae90c7d6757a492bcdf0b7bd24
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-02-02 16:47:39 +0200

    kata complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign,no-console,no-loop-func,no-unused-vars */
 const ROMAN_GLYPHS = [
   [1000, 'M',],
   [900, 'CM',],
@@ -13,10 +14,55 @@ const ROMAN_GLYPHS = [
   [4, 'IV',],
   [1, 'I',],
 ];
+const BREAK_EXCEPTION = 'break_exception';
 
 class RomanNumeralConverter {
   convert (value) {
-
+    if (!isNaN(parseInt(value,10))) {
+      if (value <= 0) throw new Error(`Value ${value} should be greater than 0`);
+      if (value > 3999) throw new Error(`Value ${value} should be less than 3999`);
+      return this.convertArabicToRoman(value);
+    } else if (typeof value === 'string') {
+      if (value === '') throw new Error('Value should not be empty');
+      if (!this.isRomanNumber(value)) throw new Error('Value should be a Roman Character');
+      return this.convertRomanToArabic(value);
+    }
+    throw new Error(`Value must be a number or String not ${typeof value}`);
+  }
+  convertArabicToRoman (value) {
+    let result = '';
+    ROMAN_GLYPHS.forEach(([val, char,]) => {
+      while (value >= val) {
+        result += char;
+        value = value - val;
+      }
+    });
+    return result;
+  }
+  convertRomanToArabic (value) {
+    let result = 0;
+    while (value !== '') {
+      try {
+        ROMAN_GLYPHS.forEach(([val, char,]) => {
+          const tester = new RegExp(`^${char}`);
+          if (tester.test(value)) {
+            result += val;
+            value = value.replace(tester, '');
+            throw BREAK_EXCEPTION;
+          }
+        });
+      } catch (error) {
+        if (error !== BREAK_EXCEPTION) {
+          throw error;
+        }
+      }
+    }
+    return result;
+  }
+  isRomanNumber (value) {
+    const valueChars = value.split('');
+    const romanChars = ROMAN_GLYPHS.map(([_, char,]) => char);
+    return valueChars.every(char => romanChars.includes(char));
   }
 }
 export default RomanNumeralConverter;
--- src/index.test.js
+++ src/index.test.js
@@ -21,10 +21,134 @@ describe('RomanNumerals', () => {
     });
   });
   describe('convert from arabic to roman', () => {
-    
+    it('should convert 1 to I', () => {
+      expect(converter.convert(1)).to.equal('I');
+    });
+    it('should convert 2 to II', () => {
+      expect(converter.convert(2)).to.equal('II');
+    });
+    it('should convert 3 to III', () => {
+      expect(converter.convert(3)).to.equal('III');
+    });
+    it('should convert 4 to IV', () => {
+      expect(converter.convert(4)).to.equal('IV');
+    });
+    it('should convert 5 to V', () => {
+      expect(converter.convert(5)).to.equal('V');
+    });
+    it('should convert 9 to IX', () => {
+      expect(converter.convert(9)).to.equal('IX');
+    });
+    it('should convert 10 to X', () => {
+      expect(converter.convert(10)).to.equal('X');
+    });
+    it('should convert 40 to XL', () => {
+      expect(converter.convert(40)).to.equal('XL');
+    });
+    it('should convert 50 to L', () => {
+      expect(converter.convert(50)).to.equal('L');
+    });
+    it('should convert 90 to XC', () => {
+      expect(converter.convert(90)).to.equal('XC');
+    });
+    it('should convert 100 to C', () => {
+      expect(converter.convert(100)).to.equal('C');
+    });
+    it('should convert 400 to CD', () => {
+      expect(converter.convert(400)).to.equal('CD');
+    });
+    it('should convert 500 to D', () => {
+      expect(converter.convert(500)).to.equal('D');
+    });
+    it('should convert 900 to CM', () => {
+      expect(converter.convert(900)).to.equal('CM');
+    });
+    it('should convert 1000 to M', () => {
+      expect(converter.convert(1000)).to.equal('M');
+    });
+    it('should convert 3999 to MMMCMXCIX', () => {
+      expect(converter.convert(3999)).to.eql('MMMCMXCIX');
+    });
+    it('should throw error if value is 0', () => {
+      expect(() => converter.convert(0)).to.throw(Error);
+    });
+    it('should throw error if value is less than 0', () => {
+      expect(() => converter.convert(-1)).to.throw(Error);
+    });
+    it('should convert 3999 to MMMCMXCIX', () => {
+      expect(converter.convert(3999)).to.eql('MMMCMXCIX');
+    });
+    it('should throw error if value is greater than 3999', () => {
+      expect(() => converter.convert(4000)).to.throw(Error);
+    });
+    it('should throw error if value is not a number or a string', () => {
+      expect(() => converter.convert(true)).to.throw(Error);
+    });
   });
 
   describe('convert from roman to arabic', () => {
-
+    it('should convert I to 1', () => {
+      expect(converter.convert('I')).to.eql(1);
+    });
+    it('should convert II to 2', () => {
+      expect(converter.convert('II')).to.eql(2);
+    });
+    it('should convert IV to 4', () => {
+      expect(converter.convert('IV')).to.eql(4);
+    });
+    it('should convert V to 5', () => {
+      expect(converter.convert('V')).to.eql(5);
+    });
+    it('should convert VI to 6', () => {
+      expect(converter.convert('VI')).to.eql(6);
+    });
+    it('should convert IX to 9', () => {
+      expect(converter.convert('IX')).to.eql(9);
+    });
+    it('should convert X to 10', () => {
+      expect(converter.convert('X')).to.eql(10);
+    });
+    it('should convert XX to 20', () => {
+      expect(converter.convert('XX')).to.eql(20);
+    });
+    it('should convert XL to 40', () => {
+      expect(converter.convert('XL')).to.eql(40);
+    });
+    it('should convert L to 50', () => {
+      expect(converter.convert('L')).to.eql(50);
+    });
+    it('should convert XC to 90', () => {
+      expect(converter.convert('XC')).to.eql(90);
+    });
+    it('should convert C to 100', () => {
+      expect(converter.convert('C')).to.eql(100);
+    });
+    it('should convert CD to 400', () => {
+      expect(converter.convert('CD')).to.eql(400);
+    });
+    it('should convert D to 500', () => {
+      expect(converter.convert('D')).to.eql(500);
+    });
+    it('should convert CM to 900', () => {
+      expect(converter.convert('CM')).to.eql(900);
+    });
+    it('should convert M to 1000', () => {
+      expect(converter.convert('M')).to.eql(1000);
+    });
+    it('should convert MMXIII to 2013', () => {
+      expect(converter.convert('MMXIII')).to.eql(2013);
+    });
+    it('should convert MMMCMXCIX to 3999', () => {
+      expect(converter.convert('MMMCMXCIX')).to.eql(3999);
+    });
+    it('should throw error if value is empty string', () => {
+      expect(() => converter.convert('')).to.throw(Error);
+    });
+    it('should throw error if value is less than 1', () => {
+      expect(() => converter.convert('-MMXIII')).to.throw(Error);
+    });
+    it('should throw error if value is not a Roman Number', () => {
+      expect(() => converter.convert('AA')).to.throw(Error);
+    });
   });
 });
