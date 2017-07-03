fa0626abc2b93f218d73273dc5404b3b4a3647db
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-07-03 13:54:10 +0300

    Sollution

--- src/index.js
+++ src/index.js
@@ -15,9 +15,65 @@ const ROMAN_GLYPHS = [
   [1, 'I',],
 ];
 
+const isRomanNumber = (value) => {
+  const chars = value.split('');
+  const romanChars = ROMAN_GLYPHS.map(([arabic, roman]) => roman)
+  return chars.every(char => romanChars.indexOf(char) >= 0);
+}
+
+const convertArabicToRoman = (value) => {
+  if (value > 3999 || value < 0) {
+    throw new Error('Value must be less than 4000')
+  }
+  let res = '';
+  while (value > 0) {
+    try {
+      ROMAN_GLYPHS.forEach(([roman, arabic]) => {
+        if (value >= roman) {
+          res += arabic;
+          value -= roman;
+          throw 'found';
+        }
+      });
+    } catch (e) {
+      if (e!=='found') {
+        throw e;
+      }
+    }
+  }
+  return res;
+}
+
+const convertRomanToArabic = (value) => {
+  if (!isRomanNumber(value)) {
+    throw new Error(`${value} is not a Roman Number`);
+  }
+  let res = 0;
+  while (value.length > 0) {
+    try {
+      ROMAN_GLYPHS.forEach(([roman, arabic]) => {
+        const testRegEx = new RegExp(`^${arabic}`)
+        if (testRegEx.test(value)) {
+          res += roman;
+          value = value.replace(testRegEx, '');
+          throw 'found';
+        }
+      });
+    } catch (e) {
+      if (e!=='found') {
+        throw e;
+      }
+    }
+  };
+  return res;
+}
+
 class RomanNumeralConverter {
   convert (value) {
-
+    if (isNaN(parseInt(value))) {
+      return convertRomanToArabic(value);
+    }
+    return convertArabicToRoman(value);
   }
 }
 export default RomanNumeralConverter;
