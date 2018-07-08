4d31894f981eee04c833e7c09940a97eece07be2
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2018-07-08 16:42:02 +0200

    sollution

--- src/index.js
+++ src/index.js
@@ -15,9 +15,46 @@ const ROMAN_GLYPHS = [
   [1, 'I',],
 ];
 
+const toRoman = (value) => {
+  if (value <= 0 || value > 3999) {
+    throw new Error('Value must be greater than 0');
+  }
+  let result = '';
+  ROMAN_GLYPHS.forEach(([arabic, roman]) => {
+    while (value >= arabic) {
+      result += roman;
+      value -= arabic;
+    }
+  });
+  return result;
+}
+
+const toArabic = (value) => {
+  let preValue = value;
+  let result = 0;
+  while (!!value) {
+    preValue = value;
+    ROMAN_GLYPHS.forEach(([arabic, roman]) => {
+      const reg = new RegExp(`^${roman}`)
+      if (reg.test(value)) {
+        result += arabic;
+        value = value.replace(reg, '');
+      }
+    });
+    if (preValue === value || result > 3999) {
+      throw new Error(`Could not convert roman to arabic`);
+    }
+  }
+  return result;
+};
+
 class RomanNumeralConverter {
   convert (value) {
-
+    if (isNaN(parseInt(value, 10))) {
+      return toArabic(value.toUpperCase());
+    }
+    return toRoman(value);
   }
 }
+
 export default RomanNumeralConverter;
--- src/index.test.js
+++ src/index.test.js
@@ -21,7 +21,32 @@ describe('RomanNumerals', () => {
     });
   });
   describe('convert from arabic to roman', () => {
-    
+    it('should convert from arabic to roman', () => {
+      expect(() => converter.convert(0)).to.throw(Error);
+      expect(converter.convert(1)).to.be.equal('I');
+      expect(converter.convert(2)).to.be.equal('II');
+      expect(converter.convert(4)).to.be.equal('IV');
+      expect(converter.convert(5)).to.be.equal('V');
+      expect(converter.convert(6)).to.be.equal('VI');
+      expect(converter.convert(9)).to.be.equal('IX');
+      expect(converter.convert(10)).to.be.equal('X');
+      expect(converter.convert(2018)).to.be.equal('MMXVIII');
+      expect(converter.convert(3999)).to.be.equal('MMMCMXCIX');
+      expect(() => converter.convert(4000)).to.be.throw(Error);
+    });
+
+    it('should convert from roman to arabic', () => {
+      expect(converter.convert('I')).to.be.equal(1);
+      expect(converter.convert('II')).to.be.equal(2);
+      expect(converter.convert('IV')).to.be.equal(4);
+      expect(converter.convert('V')).to.be.equal(5);
+      expect(converter.convert('VI')).to.be.equal(6);
+      expect(converter.convert('X')).to.be.equal(10);
+      expect(converter.convert('MMXVIII')).to.be.equal(2018);
+      expect(converter.convert('x')).to.be.equal(10);
+      expect(converter.convert('MMMCMXCIX')).to.be.equal(3999);
+      expect(() => converter.convert('a')).to.be.throw(Error);
+    })
   });
 
   describe('convert from roman to arabic', () => {
