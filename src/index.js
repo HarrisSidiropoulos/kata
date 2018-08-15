47c302483ab0ddd47447bd9bf5bbb9e32704bf99
harris sidiropoulos <hsidiropoulos@travix.com>
2018-08-15 21:32:16 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
 /* eslint-disable no-param-reassign,no-console,no-loop-func,no-unused-vars */
+const ROMAN_GLYPHS = [
+  [1000, 'M',],
+  [900, 'CM',],
+  [500, 'D',],
+  [400, 'CD',],
+  [100, 'C',],
+  [90, 'XC',],
+  [50, 'L',],
+  [40, 'XL',],
+  [10, 'X',],
+  [9, 'IX',],
+  [5, 'V',],
+  [4, 'IV',],
+  [1, 'I',],
+]
+
+const isRoman = (value) => {
+  const regx = /^(?=[MDCLXVI])M{0,3}(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$/
+  return regx.test(value)
+}
+
+const toRoman = (value) => {
+  if (value <= 0 || value > 3999) {
+    throw new Error('Cannot convert numbers great than 3999 or less than 1')
+  }
+  let res = ''
+  ROMAN_GLYPHS.forEach(([arabic, roman]) => {
+    while (value >= arabic) {
+      res += roman
+      value -= arabic
+    }
+  })
+  return res
+}
+
+const toArabic = (value) => {
+  if (!isRoman(value)) {
+    throw new Error('Value is not a Roman number')
+  }
+  let res = 0;
+  while(!!value) {
+    ROMAN_GLYPHS.forEach(([arabic, roman]) => {
+      const regx = new RegExp(`^${roman}`)
+      if (regx.test(value)) {
+        value = value.replace(regx, '')
+        res += arabic
+      }
+    })
+  }
+  return res
+}
+
+const convert = (value) => {
+  if (isNaN(parseInt(value, 10))) {
+    return toArabic(value.toUpperCase())
+  }
+  return toRoman(value)
+}
+
+export default convert
--- src/index.test.js
+++ src/index.test.js
@@ -1,1 +1,67 @@
+import convert from './index';
 
+describe('Roman Numeral Converter', () => {
+  it('should have function convert', () => {
+    expect(typeof convert).toBe('function')
+  })
+
+  describe('Arabic to roman conversion', () => {
+    it('should convert 1 to I', () => {
+      expect(convert(1)).toEqual('I')
+    })
+    it('should convert 2 to II', () => {
+      expect(convert(2)).toEqual('II')
+    })
+    it('should convert 4 to IV', () => {
+      expect(convert(4)).toEqual('IV')
+    })
+    it('should convert 5 to V', () => {
+      expect(convert(5)).toEqual('V')
+    })
+    it('should convert 6 to VI', () => {
+      expect(convert(6)).toEqual('VI')
+    })
+    it('should convert 9 to IX', () => {
+      expect(convert(9)).toEqual('IX')
+    })
+    it('should convert 10 to X', () => {
+      expect(convert(10)).toEqual('X')
+    })
+    it('should convert 20 to XX', () => {
+      expect(convert(20)).toEqual('XX')
+    })
+    it('should convert 2018 to MMXVIII', () => {
+      expect(convert(2018)).toEqual('MMXVIII')
+    })
+    it('should convert 4000 to throw an error', () => {
+      expect(() => convert(4000)).toThrow(Error)
+    })
+    it('should convert 0 to throw an error', () => {
+      expect(() => convert(0)).toThrow(Error)
+    })
+  })
+
+  describe('Roman to Arabic conversion', () => {
+    it('should convert A to throw an error', () => {
+      expect(() => convert('A')).toThrow(Error)
+    })
+    it('should convert I to 1', () => {
+      expect(convert('I')).toEqual(1)
+    })
+    it('should convert MMXVIII to 2018', () => {
+      expect(convert('MMXVIII')).toEqual(2018)
+    })
+    it('should convert MMMM to throw an Error', () => {
+      expect(() => convert('MMMM')).toThrow(Error)
+    })
+  })
+})
+
+// 3 = III
+// 4 = IV
+// 5 = V
+// 6 = VI
+// 7 = VII
+// 8 = VIII
+// 9 = IX
+// 10 = X
